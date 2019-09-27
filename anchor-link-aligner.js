1b2508e939f8609568f559fb468d5ad3acba120d
Daniel Santos <me@danielsantos.ga>
2019-09-27 10:19:52 +0100

    refactoring

--- anchor-link-aligner.js
+++ anchor-link-aligner.js
@@ -1,10 +1,16 @@
 // FUNCTIONS
 
-function aligningAction(hashOfURL) {
-
+function aligningAction(hashOfURL, event) {
+    
+    // if there is no hash (in the link), no need to align
     if(! hashOfURL) {
         return
     }
+    
+    // this is required because the browser would align the page, without taking into account the top fixed nav bar, after we already have done the correct scroll alignment. Without this lines, it would destroy the correct scroll alignment
+    event.preventDefault()
+    event.stopPropagation()
+    event.stopImmediatePropagation()
 
     const elementThatTheAnchorTargets = document.querySelector(hashOfURL) || topBarSelector
     
@@ -33,16 +39,16 @@ const topBarHeight = topBarElement.offsetHeight
 
 // 1. ON-LOAD (to address 'typing in the browser's address bar' when we are coming from outside of our website)
 
-window.onload = function() {
-    aligningAction(location.hash)
-}
+window.addEventListener('load', function(event) {
+    aligningAction(location.hash, event)
+})
 
 
-// 2. ON-POPSTATE (to address the action of "history changing" within our website)
+// 2. ON-POPSTATE (to address the action of "history changing" or "moving to a hash on the same page" within our website)
 
-window.onpopstate = function() {
-    aligningAction(location.hash)
-}
+window.addEventListener('popstate', function(event) {
+    aligningAction(location.hash, event)
+})
 
 // 3. ON-CLICK (to address clicking on anchor links of our website)
 
@@ -51,21 +57,18 @@ const anchorLinks = document.querySelectorAll('a[href*="#"]')
 
 // apply an event to those links
 for (let anchorElement of anchorLinks) {
-    anchorElement.addEventListener('click', function(e) {
+    anchorElement.addEventListener('click', function(event) {
 
         // if not the same page; quit
         if (location.pathname !== this.pathname) {
             return
         }
 
-        aligningAction(this.hash)
+        aligningAction(this.hash, event)
 
         // make the click on this links part of history (to be able to retrogress) These lines are needed because of the .preventDefault() and/or(?) .stopPropogation() code below
         const targetHref = `${location.origin}${location.pathname}${this.hash}`
         history.pushState(null, null, targetHref)
 
-        // this is required because the browser would align the page, without taking into account the top fixed nav bar, after we already have done the correct scroll alignment. Without this lines, it would destroy the correct scroll alignment
-        e.preventDefault()
-        e.stopPropagation()
     })
 }
